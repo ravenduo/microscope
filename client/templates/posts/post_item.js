cac15a2fcd893e5d624ed165f75fb935d607748f
Bob Vonderau <r.vonderau@gmail.com>
2014-12-08 16:18:17 -0600

    Adding edit post functionality and template

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,4 +1,7 @@
 Template.postItem.helpers({
+  ownPost: function() {
+    return this.userId === Meteor.usreId();
+  },
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
